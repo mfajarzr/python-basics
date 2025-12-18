eddde68a45f0504c73dcac155de727ed10b6384e
Muhammad Fajar ZR <fajar.zr@gmail.com>
2025-12-18 16:26:25 +0800

    first commit

---
+++ Variable.py
@@ -0,0 +1,21 @@
+# Variables = a container of value
+
+# string = It could be alphabets, numbers like 1234567890 and symbols
+
+first_name = "Muhammad"
+last_name = "F@jar123"
+
+# integer = true numbers
+
+integer = 123
+
+# float = decimal numbers
+
+float = 12.3
+
+# boolean = data variable type that only have 2 value: True and False
+
+bool_t = True
+bool_f = False
+
+print(type(bool_t))
